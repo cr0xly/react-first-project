fa6a487f0f3d47bf7b184bf1a3962477c7ccace7
timothy <timothy.allen@student.sl.on.ca>
2026-09-24 15:26:08 -0400

    Added optional value to interface property and assigned a default value to show how it is done.

--- src/Components/DiamondCard.tsx
+++ src/Components/DiamondCard.tsx
@@ -1,21 +1,26 @@
 interface DiamondCardProps {
     image: string
-    productName: string
+    productName?: string
     price: string
     sale?: boolean
 }
-export default function DiamondCard(props:DiamondCardProps){
+export default function DiamondCard({
+    image,
+    productName = "NO NAME", // default value for productName accompanied with the optional interface property
+    price,
+    sale
+}: DiamondCardProps){
     return (
         <div className="DiamondCard">
             <img 
-            src={props.image} 
+            src={image} 
             alt="ring1"
             width="100px"
             />
-            <h2>{props.productName}</h2>
-            <p>{props.price}</p>
+            <h2>{productName}</h2>
+            <p>{price}</p>
             <p style={{color: "red", fontWeight: "bolder"}}>
-                {props.sale && "SALE"}
+                {sale && "SALE"}
             </p>
         </div>
     )
